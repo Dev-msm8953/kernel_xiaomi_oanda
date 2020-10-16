da56ba52b7fc6f87a1f7dec0794047fa704c147d
Elena Petrova <lenaptr@google.com>
2020-10-15 20:13:35 -0700

    UPSTREAM: sched.h: drop in_ubsan field when UBSAN is in trap mode
    in_ubsan field of task_struct is only used in lib/ubsan.c, which in its
    turn is used only `ifneq ($(CONFIG_UBSAN_TRAP),y)`.
    
    Removing unnecessary field from a task_struct will help preserve the ABI
    between vanilla and CONFIG_UBSAN_TRAP'ed kernels.  In particular, this
    will help enabling bounds sanitizer transparently for Android's GKI.
    
    Signed-off-by: Elena Petrova <lenaptr@google.com>
    Change-Id: I48651ed4dbffaca0f326f05ee699277d31776d52
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Acked-by: Kees Cook <keescook@chromium.org>
    Cc: Jann Horn <jannh@google.com>
    Link: https://lkml.kernel.org/r/20200910134802.3160311-1-lenaptr@google.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 5cf53f3ce3b9ff5321b56f9ed9d90d59307be7d0)
    Signed-off-by: Elena Petrova <lenaptr@google.com>
    Change-Id: I48651ed4dbffaca0f326f05ee699277d31776d52
    (cherry picked from commit a04bf2260f85ccbe122414e168d38d42a1d8b850)
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1989,7 +1989,7 @@ struct task_struct {
 	struct held_lock held_locks[MAX_LOCK_DEPTH];
 	gfp_t lockdep_reclaim_gfp;
 #endif
-#ifdef CONFIG_UBSAN
+#if defined(CONFIG_UBSAN) && !defined(CONFIG_UBSAN_TRAP)
 	unsigned int in_ubsan;
 #endif
 
