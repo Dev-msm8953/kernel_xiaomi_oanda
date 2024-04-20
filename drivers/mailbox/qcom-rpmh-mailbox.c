7b2f0e4dd3db34e66ef7f72b1eabf00f87073987
AkumaHunt3r <AkumaHunt3r@protonmail.com>
2024-04-20 10:16:12 +0000

    mailbox: qcom: Please shut up ty
    Change-Id: a4a46f43ecaee2ac5761b7b617d097214ea4c4c2
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- drivers/mailbox/qcom-rpmh-mailbox.c
+++ drivers/mailbox/qcom-rpmh-mailbox.c
@@ -999,11 +999,8 @@ tx_fail:
 	}
 
 	/* If we were just busy waiting for TCS, dump the state and return */
-	if (ret == -EBUSY) {
-		dev_err_ratelimited(chan->cl->dev,
-				"TCS Busy, retrying RPMH message send\n");
+	if (ret == -EBUSY)
 		ret = -EAGAIN;
-	}
 
 	return ret;
 }
